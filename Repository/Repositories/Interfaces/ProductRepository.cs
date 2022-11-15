4ffa6bbf7088b29bcd0fa45344457d8b1a6b61d2
toghrulvg <toghrulvg@code.edu.az>
2022-11-16 01:54:38 +0400

    added

---
+++ Repository/Repositories/Interfaces/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories.Interfaces
+{
+    public class ProductRepository : Repository<Product> , IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base (context )
+        { 
+        }
+    }
+}
